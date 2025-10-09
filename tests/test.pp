9f82b4c7344212ac6b58e353fd5223762bceeca0
Tobias Rang <tobias.rang@redpill-linpro.com>
2025-10-09 22:50:57 +0200

    Add new dsl features to test policy

--- tests/test.pp
+++ tests/test.pp
@@ -1,11 +1,17 @@
+
+# func def
 def f(x)
     return x * 2
 end
 
+# list construction and assignment
 l = [1,2]
-# print(l.map(f))
+
+# bound methods
+print(l.map(f))
 
 
+# if stmt
 def test_if_else(x)
     if x == "root"
         print("greetings, allmighty root")
@@ -17,10 +23,12 @@ def test_if_else(x)
     return nil
 end
 
+# for loop
 for n in ["user", "root"]:
     test_if_else(n)
 end
 
+# dicts
 user = {"name": "alice"}
 user["age"] := 38
 print(user)
@@ -28,5 +36,26 @@ print(user)
 token["roles"]["client_b"] := [1,2,3]
 print(token)
 
+# range objects - wrapped python objects and bound methods
+
+for x in range(10):
+    print(x)
+end
+
+# single-expression lambdas
+
+print([1, 2, 3].map(i => i * i))
+
+# multi statement lambdas
+
+g = (a, b) =>
+    print(a)
+    print(b)
+    return a + b
+end
+
+print(g(10, 20))
+
+# policy return value
 return 1 in l
 
